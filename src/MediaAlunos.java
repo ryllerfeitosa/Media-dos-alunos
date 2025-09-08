841e0ddf98f3de9039cf49415eb7b68150abf512
RILLER DA COSTA FEITOSA <25203652@CFP127ALUNO.LOCAL>
2025-09-08 09:00:54 -0300

    Código do projeto

--- src/MediaAlunos.java
+++ src/MediaAlunos.java
@@ -1,4 +1,52 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class MediaAlunos {
+
+    private String nome;
+    private double nota1;
+    private double nota2;
+    private double nota3;
+    private double pesoNota1;
+    private double pesoNota2;
+    private double pesoNota3;
+    private double mediaPonderada;
+
+
+    public void obterDados(){
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Qual o nome do aluno?");
+        nome = leitor.nextLine();
+
+        System.out.println("Qual a nota 1 do aluno " + nome + "?");
+        nota1 = leitor.nextDouble();
+
+        System.out.println("Qual o peso da nota 1 do " + nome + " de 0 a 5?");
+        pesoNota1 = leitor.nextDouble();
+
+        System.out.println("Qual a nota 2 do aluno " + nome + "?");
+        nota1 = leitor.nextDouble();
+
+        System.out.println("Qual o peso da nota 2 do " + nome + " de 0 a 5?");
+        pesoNota1 = leitor.nextDouble();
+
+        System.out.println("Qual a nota 3 do aluno " + nome + "?");
+        nota1 = leitor.nextDouble();
+
+        System.out.println("Qual o peso da nota 3 do " + nome + " de 0 a 5?");
+        pesoNota1 = leitor.nextDouble();
+
+        exibirResultado();
+        leitor.close();
+
+    }
+    public void CalcularMedia(){
+        mediaPonderada = ((nota1 * pesoNota1) + (nota2 * pesoNota2) + (nota3 * pesoNota3)) / 5;
+    }
+    public void exibirResultado(){
+
+    CalcularMedia();
+        System.out.println("A média ponderada do aluno " + nome + " é " + mediaPonderada);
+
+    }
 }
--- src/MediaAlunosApp.java
+++ src/MediaAlunosApp.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class MediaAlunosApp {
+
+    public static void main(String[] args){
+        System.out.println("Iniciando a aplicação...");
+        MediaAlunos MediaAlunos1 = new MediaAlunos();
+        MediaAlunos1.obterDados();
+    }
 }
